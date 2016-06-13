0a2946c46208a615ab6c9b931fc47119aecf265d
Steve Lee <steve@opendirective.com>
2016-06-13 15:31:07 +0100

    fixed config object

--- kinvey-tests/src/test.js
+++ kinvey-tests/src/test.js
@@ -1,15 +1,14 @@
 import test from 'tape'
 
-// setup for all tests]
-import {config} from './kinvey-config'
+import config from './kinvey-config'
 
 function runTests () {
   console.debug(config)
   console.debug(Kinvey)
 
   test('init', (assert) => {
-    const client = Kinvey.init({appkey: config.appKey,
-                                appsecret: config.appSecret})
+    const client = Kinvey.init({appKey: config.appKey,
+                                appSecret: config.appSecret})
 
     assert.equal(client instanceof Kinvey.client, true,
       'init should return a Client object')
