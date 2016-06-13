54e2458e2751483e036abe39b437fcb04301a906
Steve Lee <steve@opendirective.com>
2016-06-13 21:20:50 +0100

    Add active user test

--- kinvey-tests/src/test.js
+++ kinvey-tests/src/test.js
@@ -6,13 +6,18 @@ import config from './kinvey-config'
 const WIRE_TIMEOUT = 1000
 
 function runTests () {
+  test('Kinvey client SDK', t => {
+    const kd = KinveyDevice.toJSON()
+    t.equal(kd.kinveySDK.name, 'kinvey-html5-sdk')
+    t.comment(`[info] sdk version: ${kd.kinveySDK.version}`)
+    t.end()
+  })
+
   test('Init Kinvey client', (t) => {
     const client = Kinvey.init({appKey: config.appKey,
                                 appSecret: config.appSecret})
 
-    // init() does NOT return a Client as the docs say
-    // so just test it does not throw
-    t.pass('Did not throw')
+    t.equal(client.constructor.name, "Client")
     t.end()
   })
 
@@ -20,18 +25,24 @@ function runTests () {
     t.timeoutAfter(WIRE_TIMEOUT)
     Kinvey.ping().then((response) => {
       t.equal(config.appName, response.appName)
-      t.comment(`Kinvey version: ${response.version}`)
+      t.comment(`[info] Kinvey version: ${response.version}`)
       resolve()
     })
   }))
 
+  test('Current user', (t) => {
+    t.equal(Kinvey.User.getActiveUser(), null,
+      'Active user should be null')
+    t.end()
+  })
+
 }
 
-function execeute() {
+function execute() {
   runTests()
 
   // throwing forces browser-run to exit, else npm test hangs
   setTimeout(() => {throw "exiting"}, 3000)
 }
 
-window.onload = execeute
+window.onload = execute
